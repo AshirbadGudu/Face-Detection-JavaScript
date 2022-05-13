f7a9a25bc436d0f76d5eb9457186f329fab6f09b
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-13 08:10:31 +0530

    scripts moved to js folder

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ const startVideo = async () => {
 };
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("../models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("../models"),
 ]).then(startVideo);
 
 video.addEventListener("play", () => {
